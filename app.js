4f87bb4858f71073a9d8fd85068555a516c99fe9
lukmannx <lukmannhakimx@gmail.com>
2022-03-03 14:37:00 +0700

    alhamdulillah akhi

--- app.js
+++ app.js
@@ -1,11 +1,55 @@
-const http = require('http')
+
+const http = require('http');
+const fs = require('fs');
+
+const serper = 3000;
+
+// function panggilCSS(req, res){
+//     if (req.url === '/style.css') {
+//         res.writeHead(200,{
+//             'Content-Type' : 'text/css'
+//         });
+//         const fileContents = fs.readFileSync('/style.css', {encoding:'utf8'});
+//         res.write(fileContents);
+//         res.end();
+//     }
+// }
+
+const renderHTML = (path, res) => {
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            res.writeHead(404)
+        } else {
+            res.write(data);
+        }
+        res.end();
+    })
+}
 
 http 
     .createServer((req, res) => {
-        res.write('hello tod');
-        res.end();
+        res.writeHead(200, {
+            'Content-Type':'text/html',
+        });
+        //res.write('hello tod');
+        const url = req.url;
+
+        switch(url){
+            case '/':
+                renderHTML('./index.html', res);
+                break;
+            case '/about':
+                renderHTML('./about.html', res);
+                break;
+            case '/form':
+                renderHTML('./form.html', res);
+                break;
+            default:
+                renderHTML('./home.html', res)
+                break;
+        }
     })
 
-    .listen(3000, () => {
-        console.log('server jalan tod')
+    .listen(serper, () => {
+        console.log('server jalan bangg')
     })
